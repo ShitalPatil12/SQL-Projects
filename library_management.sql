6adea9d8f41f095fa78f138eb7a09e54245aa6aa
ShitalPatil12 <74902816+ShitalPatil12@users.noreply.github.com>
2024-03-17 04:14:35 +0530

    Update library_management.sql

--- library_management.sql
+++ library_management.sql
@@ -1,41 +1,38 @@
-DROP SCHEMA IF EXISTS library_management;
-CREATE SCHEMA IF NOT EXISTS library_management;
-
 DROP TABLE IF EXISTS library_management.user_login;
 CREATE TABLE IF NOT EXISTS library_management.user_login (
-	user_id TEXT PRIMARY KEY,
-    user_password TEXT,
-    first_name TEXT,
-	last_name TEXT,
-	sign_up_on DATE,
-	email_id TEXT
+	user_id VARCHAR(50) PRIMARY KEY,
+    user_password VARCHAR(50),
+    first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	sign_up_on VARCHAR(50),
+	email_id VARCHAR(50)
 );
 
 DROP TABLE IF EXISTS library_management.publisher;
 CREATE TABLE IF NOT EXISTS library_management.publisher (
-	publisher_id TEXT PRIMARY KEY,
-    publisher TEXT,
-    distributor TEXT,
+	publisher_id VARCHAR(50) PRIMARY KEY,
+    publisher VARCHAR(50),
+    distributor VARCHAR(50),
 	releases_count INT,
 	last_release DATE
 );
 
 DROP TABLE IF EXISTS library_management.author;
 CREATE TABLE IF NOT EXISTS library_management.author (
-	author_id TEXT PRIMARY KEY,
-	first_name TEXT,
-	last_name TEXT,
+	author_id VARCHAR(50) PRIMARY KEY,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
 	publications_count INT
 );
 
 DROP TABLE IF EXISTS library_management.books;
 CREATE TABLE IF NOT EXISTS library_management.books (
-	book_id TEXT PRIMARY KEY,
-    book_code TEXT,
-    book_name TEXT,
-    author_id TEXT REFERENCES library_management.author (author_id),
-	publisher_id TEXT REFERENCES library_management.publisher (publisher_id),
-    book_version TEXT,
+	book_id VARCHAR(50) PRIMARY KEY,
+    book_code VARCHAR(50),
+    book_name VARCHAR(50),
+    author_id VARCHAR(50) REFERENCES library_management.author (author_id),
+	publisher_id VARCHAR(50) REFERENCES library_management.publisher (publisher_id),
+    book_version VARCHAR(50),
     release_date DATE,
     available_from DATE,
     is_available BOOLEAN
@@ -43,10 +40,10 @@ CREATE TABLE IF NOT EXISTS library_management.books (
 
 DROP TABLE IF EXISTS library_management.staff;
 CREATE TABLE IF NOT EXISTS library_management.staff (
-	staff_id TEXT PRIMARY KEY,
-    first_name TEXT,
-	last_name TEXT,
-    staff_role TEXT,
+	staff_id VARCHAR(50) PRIMARY KEY,
+    first_name VARCHAR(50),
+	last_name VARCHAR(50),
+    staff_role VARCHAR(50),
     start_date DATE,
     last_date DATE,
     is_active BOOLEAN,
@@ -56,9 +53,9 @@ CREATE TABLE IF NOT EXISTS library_management.staff (
 
 DROP TABLE IF EXISTS library_management.readers;
 CREATE TABLE IF NOT EXISTS library_management.readers (
-	reader_id TEXT PRIMARY KEY,
-    first_name TEXT,
-	last_name TEXT,
+	reader_id VARCHAR(50) PRIMARY KEY,
+    first_name VARCHAR(50),
+	last_name VARCHAR(50),
 	registered_on DATE,
 	books_issued_total INT,
 	books_issued_current INT,
@@ -71,13 +68,13 @@ CREATE TABLE IF NOT EXISTS library_management.readers (
 DROP TABLE IF EXISTS library_management.books_issue;
 CREATE TABLE IF NOT EXISTS library_management.books_issue (
 	issue_id SERIAL PRIMARY KEY,
-	book_id TEXT REFERENCES library_management.books (book_id),
-	issued_to TEXT REFERENCES library_management.readers (reader_id),
+	book_id VARCHAR(50) REFERENCES library_management.books (book_id),
+	issued_to VARCHAR(50) REFERENCES library_management.readers (reader_id),
 	issued_on DATE,
 	return_on DATE,
 	current_fine FLOAT,
 	fine_paid BOOLEAN,
-	payment_transaction_id TEXT
+	payment_transaction_id VARCHAR(50)
 );
 
 DROP TABLE IF EXISTS library_management.settings;
